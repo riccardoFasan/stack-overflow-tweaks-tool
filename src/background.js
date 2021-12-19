70f15db81f48dba83dd93b79c4f68679aaec0e10
Riccardo Fasan <57989127+riccardoFasan@users.noreply.github.com>
2021-12-19 15:04:29 +0100

    fix: default values on install

---
+++ src/background.js
@@ -0,0 +1,6 @@
+chrome.runtime.onInstalled.addListener( () => {
+    chrome.storage.sync.set({ addButton: false });
+    chrome.storage.sync.set({ highlightAnswer: false });
+    chrome.storage.sync.set({ hideNavBar: false });
+    chrome.storage.sync.set({ hideSuggestionsBar: false });
+});
