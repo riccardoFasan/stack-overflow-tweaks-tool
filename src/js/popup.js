fcc04bedd78d1a921592db17b2d64103ac10aede
Riccardo Fasan <fasanriccardo21@gmail.com>
2022-01-13 21:44:32 +0100

    refactor: popup template made dynamic

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,4 +1,48 @@
-const options = ['addButton', 'highlightAnswer', 'hideNavBar', 'hideTooltipsBar', 'getAnswersLink'];
+const options = [
+	{
+		name: 'addButton',
+		title: 'Highlight the correct answer',
+		text: 'The correct answer, if any, will be highlighted with a green background.',
+	},
+	{
+		name: 'highlightAnswer',
+		title: 'Jump to the answer',
+		text: 'If ther\'s a correct answer, a button will be added in the question header, next to the "ask a question" button. When you click the new button, the page will scroll to the correct answer.',
+	},
+	{
+		name: 'getAnswersLink',
+		title: 'Copy answers link',
+		text: 'A # will appear next to each answer. By clicking it the link of that answer will be copied on your clickboard.',
+	},
+	{
+		name: 'hideNavBar',
+		title: 'Hide the navigation bar',
+		text: 'The left navigation will be hide in order to improve your focus.',
+	},
+	{
+		name: 'hideTooltipsBar',
+		title: 'Hide the tooltips bar',
+		text: 'The tooltips and the hot question network will be hide in order to improve your focus.',
+	},
+];
+
+const form = document.querySelector('form');
+
+function appendInput(option) {
+	const label = document.createElement('label');
+	label.setAttribute('for', option.name);
+	label.innerHTML = `
+		<div>
+			<div>${option.title}</div>
+			<p>${option.text}</p>
+		</div>
+		<div class="switch">
+			<input id="${option.name}" type="checkbox" />
+			<span class="slider"></span>
+		</div>
+	`;
+	form.appendChild(label);
+}
 
 function setStorageValue(propertyName, propertyValue) {
 	return new Promise(resolve => {
@@ -16,18 +60,19 @@ function getStorageValue(propertyName) {
 }
 
 options.forEach(option => {
-	getStorageValue(option).then(defaultValue => {
+	appendInput(option);
+	getStorageValue(option.name).then(defaultValue => {
 		if (defaultValue === undefined) {
 			defaultValue = false;
-			setStorageValue(option, defaultValue);
+			setStorageValue(option.name, defaultValue);
 		}
 
-		const input = document.querySelector(`#${option}`);
+		const input = document.querySelector(`#${option.name}`);
 		input.checked = defaultValue;
 
 		input.addEventListener('click', () => {
-			getStorageValue(option).then(currentValue => {
-				setStorageValue(option, !currentValue);
+			getStorageValue(option.name).then(currentValue => {
+				setStorageValue(option.name, !currentValue);
 			});
 		});
 	});
