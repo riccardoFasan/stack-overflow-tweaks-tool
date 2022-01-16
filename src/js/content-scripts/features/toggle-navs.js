7618ac0853e5d8457a8b5939d4aa4abfa95ee7a5
Riccardo Fasan <57989127+riccardoFasan@users.noreply.github.com>
2022-01-16 11:27:21 +0100

    refactor: navs.js moved to toggle-navs.js

--- src/js/content-scripts/features/toggle-navs.js
+++ src/js/content-scripts/features/toggle-navs.js
@@ -1,11 +1,11 @@
 function toggleNavigationBar() {
 	navigationBar.classList.toggle('w-0');
-    container.classList.toggle('no-border');
+	container.classList.toggle('no-border');
 }
 
 function toggleTooltipsBar() {
 	tooltipsBar.classList.toggle('d-none');
-    content.classList.toggle('w-100');
+	content.classList.toggle('w-100');
 }
 
 const HideNavBar = new Feature('hideNavBar', toggleNavigationBar, toggleNavigationBar);
