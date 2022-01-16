b87a98c27b1e503fe5d17fa9f004ec4d8252ee51
Riccardo Fasan <57989127+riccardoFasan@users.noreply.github.com>
2022-01-16 16:35:57 +0100

    build: firefox version reach 1.4.0

--- src/js/content-scripts/common.js
+++ src/js/content-scripts/common.js
@@ -36,10 +36,10 @@ setGreen();
 
 injectableIcons.forEach(icon => {
 	setStyleVariable(icon.name, `url('${getImageURL(icon.path)}')`);
-})
+});
 
 function getImageURL(path) {
-	return chrome.runtime.getURL(path);
+	return browser.runtime.getURL(path);
 }
 
 function setStyleVariable(name, value) {
