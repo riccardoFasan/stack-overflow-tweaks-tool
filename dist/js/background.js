e6227f5fe9a7be21ac839a959f076181079d18e4
Riccardo Fasan <57989127+riccardoFasan@users.noreply.github.com>
2022-04-03 18:43:48 +0200

    fix: added dist to gitignore

--- dist/js/background.js
+++
@@ -1,8 +0,0 @@
-browser.runtime.onInstalled.addListener(() => {
-	browser.storage.sync.set({ jumpToTheAnswer: false });
-	browser.storage.sync.set({ highlightAnswer: false });
-	browser.storage.sync.set({ getAnswersLink: false });
-	browser.storage.sync.set({ copyCode: false });
-	browser.storage.sync.set({ hideNavBar: false });
-	browser.storage.sync.set({ hideTooltipsBar: false });
-});
--- dist/js/content-scripts/features/feature.js
+++
@@ -1,7 +0,0 @@
-class Feature {
-	constructor(name, enable, disable) {
-		this.name = name;
-		this.enable = enable;
-		this.disable = disable;
-	}
-}
