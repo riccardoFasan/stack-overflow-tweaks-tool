f3cacd4684796c6395661575218967a6ec636f91
Gustavo Mauricio de Barros <gustavomauriciodebarros@gmail.com>
2022-11-04 18:52:19 -0300

    Fixed the copy function

--- src/js/content-scripts/features/copy-code.js
+++ src/js/content-scripts/features/copy-code.js
@@ -24,23 +24,9 @@ function addCopyButton(block) {
 	const code = block.querySelector('code');
 
 	button.addEventListener('click', () => {
-		const plainText = getPlainText(code.innerHTML);
+		const plainText = code.innerText;
 		copyToClipboard(plainText);
 	});
-}
-
-function getPlainText(html) {
-	//stackoverflow.com//questions/15180173/convert-html-to-plain-text-in-js-without-browser-environment#answer-20071776
-	html = html.replace(/<style([\s\S]*?)<\/style>/gi, '');
-	html = html.replace(/<script([\s\S]*?)<\/script>/gi, '');
-	html = html.replace(/<\/div>/gi, '\n');
-	html = html.replace(/<\/li>/gi, '\n');
-	html = html.replace(/<li>/gi, '  *  ');
-	html = html.replace(/<\/ul>/gi, '\n');
-	html = html.replace(/<\/p>/gi, '\n');
-	html = html.replace(/<br\s*[\/]?>/gi, '\n');
-	html = html.replace(/<[^>]+>/gi, '');
-	return html;
 }
 
 function removeCopyButtons() {
